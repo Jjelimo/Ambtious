76541c2d815e214278cb5b15987f4bdcc1b3dc7d
Jjelimo <jolynejelimo@gmail.com>
2017-03-06 18:16:31 +0300

    trial

---
+++ prime numbers .py
@@ -0,0 +1,14 @@
+def primechecker(numbers):
+	for number in numbers:
+		if number<2:
+			print ("It is not prime number")
+		elif number==2:
+			print ("it is a prime number")
+		elif number%2==0:
+			print ("it is an even number but  not a prime number")
+		elif number%2:
+			print ("it is an odd number but not a prime number")
+		else:
+		    print("it is a prime number")
+		print(number)
+		
