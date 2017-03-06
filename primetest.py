6565fdf5f4cc2db7e7cbeb7d8c0815294b7019b0
Jjelimo <jolynejelimo@gmail.com>
2017-03-06 19:06:51 +0300

    Final'

--- primetest.py
+++ primetest.py
@@ -2,11 +2,13 @@ import unittest
 from prime_numbers import primechecker
 class testresults(unittest.TestCase):
 	def test_numbers_generated_are_prime_numbers(self):
-		self.assert True( primechecker(10), 3)
+		self.assert True( primechecker(100),2)
 	def test_number_is_prime_number(self):
 	    self.assert True (primechecker(10),0)
 	def test_number_is_prime_number(self):
-		self.assert True (primechecker(1000),8)
+		self.assert True (primechecker(1000),867)
 	def test_number_is_prime_number (self):
-        self.assert True (primechecker(-100),-8)
+        self.assert True (primechecker(-100),-10)
+
+
         
